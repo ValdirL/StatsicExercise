a55218bb942cc6b1d271aa0bae1eb7c43bec0c07
Valdir Lima <valdirlima508@gmail.com>
2022-02-24 11:04:43 -0500

    initial commit

---
+++ StaticExercise/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace StaticExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Don't need to do this since its a static class - TempConverter tempconverter = new TempConverter();  
+            TempConverter.FahrenheitToCelcius(9.5);
+            TempConverter.CelsiusToFahrenheit(9.5);
+        }
+    }
+}
---
+++ StaticExercise/TempConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticExercise
+{
+    public static class TempConverter
+    {
+        public static double FahrenheitToCelcius(double fahrenheit) 
+        { 
+            return (fahrenheit - 32) / 1.8000; 
+        }
+
+        public static double CelsiusToFahrenheit(double celcius) 
+        { 
+            return (celcius * 9 / 5) + 32; 
+        }
+    }
+}
